175b5b4b03ca80cce1bc5d9012ae11fd036f6f7f
Tina Wang <tina@tina.cafe>
2021-05-10 23:06:30 +0200

    ✏️ Change page title & favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,11 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Contrast',
+    title: 'Nexon',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Contrast' }
+      { hid: 'description', name: 'description', content: 'Nexon' }
     ],
     htmlAttrs: {
       lang: 'en'
